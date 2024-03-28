b1c262a1ce32bed7c72e1f0bf0fc1a0738080864
jrcalgo <95605366+jrcalgo@users.noreply.github.com>
2024-03-28 14:37:55 -0400

    mouse collector class skeleton

--- lib/smartDIR/learning/data_collection/MouseTimeSeriesCollector.py
+++ lib/smartDIR/learning/data_collection/MouseTimeSeriesCollector.py
@@ -9,22 +9,33 @@ import matplotlib.pyplot as plt
 from pynput import mouse as ms
 
 class Collect_Mouse_Data:
-    def __init__(self, mouse_dpi, os_mouse_sensitivity):
-        self.mouse_dpi = mouse_dpi
+    def __init__(self):
+        self.MAX_PROCESSES = 5
+        self.running_processes = 0
+
+        self.mouse_dpi = os.system()
         self.os_mouse_sensitivity = os_mouse_sensitivity
         self.features = ['timestamp', 'x', 'y', 'x_velocity', 'y_velocity', 'button'] 
         self.stored_data = pd.DataFrame(columns=self.features)
 
-    def activate_collector(self):
+    def collect_data(self, activation:bool=True):
+        if activation is None:
+            raise ValueError("No activation specified.")
 
-    def deactivate_collector(self, save:bool=False):
+        if activation:
+            self.collecting = True
+        else:
+            self.collecting = False
+            try:
+                return self.listener.stop()
+            except:
+                pass
+
+    def plot_data(self, x_axis, y_axis, real_time:bool=False):
+
+    def get_data(self):
+        return self.data
 
     def save_data(self):
         with open('mouse.csv', 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldNames=self.features)
-
-    def get_current_data(self):
-
-    def plot_current_data(self):
-        
-    
+            writer = csv.DictWriter(file, fieldNames=self.features)    
